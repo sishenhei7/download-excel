e91f754dd9b86744388bc1676fe0a5355217a511
sishenhei7 <982505136@qq.com>
2019-11-22 14:48:07 +0800

    chore: modify file path

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.NODE_ENV === 'production' ? '/digit-flip/' : '/'
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/download-excel/' : '/'
 
 module.exports = {
   publicPath: BASE_URL,
